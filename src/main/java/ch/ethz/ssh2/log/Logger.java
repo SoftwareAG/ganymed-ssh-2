4eeed0860122c89403db4009cd31953bc76fbcf5
cleondris <cleondris@gmail.com>
2013-08-01 12:28:42 +0000

    Disable logging by default. Only used for development (as in earlier releases).

--- src/main/java/ch/ethz/ssh2/log/Logger.java
+++ src/main/java/ch/ethz/ssh2/log/Logger.java
@@ -2,20 +2,23 @@
  * Copyright (c) 2006-2011 Christian Plattner. All rights reserved.
  * Please refer to the LICENSE.txt for licensing details.
  */
+
 package ch.ethz.ssh2.log;
 
 import java.util.logging.Level;
 
 /**
- * Logger delegating to JRE logging.
+ * Logger delegating to JRE logging. By default, this is disabled and only
+ * used during development.
  *
  * @author Christian Plattner
  * @version $Id$
  */
 public class Logger
 {
-
 	private java.util.logging.Logger delegate;
+
+	public static volatile boolean enabled = false;
 
 	public static Logger getLogger(Class<?> x)
 	{
@@ -29,31 +32,34 @@ public class Logger
 
 	public boolean isDebugEnabled()
 	{
-		return delegate.isLoggable(Level.FINER);
+		return enabled && delegate.isLoggable(Level.FINER);
 	}
 
 	public void debug(String message)
 	{
-		delegate.fine(message);
+		if (enabled)
+			delegate.fine(message);
 	}
 
 	public boolean isInfoEnabled()
 	{
-		return delegate.isLoggable(Level.FINE);
+		return enabled && delegate.isLoggable(Level.FINE);
 	}
 
 	public void info(String message)
 	{
-		delegate.info(message);
+		if (enabled)
+			delegate.info(message);
 	}
 
 	public boolean isWarningEnabled()
 	{
-		return delegate.isLoggable(Level.WARNING);
+		return enabled && delegate.isLoggable(Level.WARNING);
 	}
 
 	public void warning(String message)
 	{
-		delegate.warning(message);
+		if (enabled)
+			delegate.warning(message);
 	}
 }
