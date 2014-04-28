dbd756230687a7f6c984e92e70abfaa71f4851f4
dkocher <dkocher@sudo.ch>
2014-04-28 09:07:29 +0000

    Implement #toString

--- src/main/java/ch/ethz/ssh2/transport/KexParameters.java
+++ src/main/java/ch/ethz/ssh2/transport/KexParameters.java
@@ -4,6 +4,8 @@
  */
 package ch.ethz.ssh2.transport;
 
+import java.util.Arrays;
+
 /**
  * KexParameters.
  * 
@@ -25,4 +27,24 @@ public class KexParameters
 	public String[] languages_server_to_client;
 	public boolean first_kex_packet_follows;
 	public int reserved_field1;
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("KexParameters{");
+        sb.append("cookie=").append(Arrays.toString(cookie));
+        sb.append(", kex_algorithms=").append(Arrays.toString(kex_algorithms));
+        sb.append(", server_host_key_algorithms=").append(Arrays.toString(server_host_key_algorithms));
+        sb.append(", encryption_algorithms_client_to_server=").append(Arrays.toString(encryption_algorithms_client_to_server));
+        sb.append(", encryption_algorithms_server_to_client=").append(Arrays.toString(encryption_algorithms_server_to_client));
+        sb.append(", mac_algorithms_client_to_server=").append(Arrays.toString(mac_algorithms_client_to_server));
+        sb.append(", mac_algorithms_server_to_client=").append(Arrays.toString(mac_algorithms_server_to_client));
+        sb.append(", compression_algorithms_client_to_server=").append(Arrays.toString(compression_algorithms_client_to_server));
+        sb.append(", compression_algorithms_server_to_client=").append(Arrays.toString(compression_algorithms_server_to_client));
+        sb.append(", languages_client_to_server=").append(Arrays.toString(languages_client_to_server));
+        sb.append(", languages_server_to_client=").append(Arrays.toString(languages_server_to_client));
+        sb.append(", first_kex_packet_follows=").append(first_kex_packet_follows);
+        sb.append(", reserved_field1=").append(reserved_field1);
+        sb.append('}');
+        return sb.toString();
+    }
 }
--- src/main/java/ch/ethz/ssh2/transport/NegotiatedParameters.java
+++ src/main/java/ch/ethz/ssh2/transport/NegotiatedParameters.java
@@ -23,4 +23,22 @@ public class NegotiatedParameters
 	public String comp_algo_server_to_client;
 	public String lang_client_to_server;
 	public String lang_server_to_client;
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("NegotiatedParameters{");
+        sb.append("guessOK=").append(guessOK);
+        sb.append(", kex_algo='").append(kex_algo).append('\'');
+        sb.append(", server_host_key_algo='").append(server_host_key_algo).append('\'');
+        sb.append(", enc_algo_client_to_server='").append(enc_algo_client_to_server).append('\'');
+        sb.append(", enc_algo_server_to_client='").append(enc_algo_server_to_client).append('\'');
+        sb.append(", mac_algo_client_to_server='").append(mac_algo_client_to_server).append('\'');
+        sb.append(", mac_algo_server_to_client='").append(mac_algo_server_to_client).append('\'');
+        sb.append(", comp_algo_client_to_server='").append(comp_algo_client_to_server).append('\'');
+        sb.append(", comp_algo_server_to_client='").append(comp_algo_server_to_client).append('\'');
+        sb.append(", lang_client_to_server='").append(lang_client_to_server).append('\'');
+        sb.append(", lang_server_to_client='").append(lang_server_to_client).append('\'');
+        sb.append('}');
+        return sb.toString();
+    }
 }
